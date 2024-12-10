4ffb24d4c58a5212d65fbb611d5fbc13beb2fe06
noe maurel <noe.maurel31@gmail.com>
2024-12-10 21:45:21 +0100

    add all

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit  } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+
+  buttonText!: string;
+  @Input() faceSnap!: FaceSnap;
+    
+  ngOnInit(){
+  this.buttonText = "Oh Snap!"
+  }
+  onSnap() {
+    if (this.buttonText == "Oh Snap!"){
+      this.faceSnap.snaps++;
+      this.buttonText = "Oops, un Snap!"
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh Snap!";
+    }
+   
+  }
+}
+
+
