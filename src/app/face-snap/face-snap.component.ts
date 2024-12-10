4928b0f527e0ec0e44223d6ae984210f5a84f04a
noe maurel <noe.maurel31@gmail.com>
2024-12-10 22:03:28 +0100

    Add style with NgClass

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit  } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
@@ -14,6 +14,7 @@ export class FaceSnapComponent implements OnInit {
 
   buttonText!: string;
   @Input() faceSnap!: FaceSnap;
+  userHasSnapped: boolean | undefined;
     
   ngOnInit(){
   this.buttonText = "Oh Snap!"
@@ -22,9 +23,11 @@ export class FaceSnapComponent implements OnInit {
     if (this.buttonText == "Oh Snap!"){
       this.faceSnap.snaps++;
       this.buttonText = "Oops, un Snap!"
+      this.userHasSnapped = true;
     } else {
       this.faceSnap.snaps--;
       this.buttonText = "Oh Snap!";
+      this.userHasSnapped = false;
     }
    
   }
